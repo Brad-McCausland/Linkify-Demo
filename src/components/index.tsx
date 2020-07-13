4e36c71ecbd396c3c381aaaa008502ab0fbbd2a4
bmcc0605 <mccausland0605@gmail.com>
2020-07-13 16:35:53 -0700

    Initial project setup

---
+++ src/components/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render()
+    {
+        return (
+            <p>huehuehue</p>
+        );
+    }
+}
+
+ReactDOM.render (
+    <App/>,
+    document.getElementById("output")
+);
